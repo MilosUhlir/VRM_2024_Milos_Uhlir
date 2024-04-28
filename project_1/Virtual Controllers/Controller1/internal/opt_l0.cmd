d9ef405e7f8a6ad51b4f659bb137dc0c48336d8b
MilosUhlir <milos.uhlir@volny.cz>
2024-04-28 21:26:22 +0200

    initial upload
    initial uplad of VRM project 1

---
+++ project_1/Virtual Controllers/Controller1/internal/opt_l0.cmd
@@ -0,0 +1,4 @@
+
+include -path "C:/Users/milda/AppData/Local/ABB/RobotWare/RobotControl_7.12.0/options/integratedIO/integratedIO_opt.cmd"
+
+include -path "C:/Users/milda/AppData/Local/ABB/RobotWare/RobotControl_7.12.0/options/sc/ScInc_opt.cmd"
